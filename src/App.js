ba418e94b91f57506cce035580148123971e4007
jibola2588 <jibolafawole258@gmail.com>
2023-03-20 11:04:22 +0100

    class on props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Practice from './classes/20-03-23'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+    render(){
+      const data = [
+        {
+          name:'Nathan',
+          age:12,
+          gender:'male',
+          btn:'nathan',
+          bgColor:'green'
+        },
+        {
+          name:'Kennedy',
+          age:31,
+          gender:'female',
+          btn:'kennedy',
+          bgColor:'red'
+        },
+        {
+          name:'Peter',
+          age:91,
+          gender:'male',
+          btn:'peter',
+          bgColor:'blue'
+        },
+        {
+          name:'Emma',
+          age:44,
+          gender:'female',
+          btn:'emma',
+          bgColor:'orange'
+        },
+      ]
+
+       return(
+        <div className='flex flex-wrap gap-4 items-center mt-2'>
+          <Practice user={data[0]}/>
+          <Practice user={data[1]}/>
+          <Practice user={data[2]}/>
+          <Practice user={data[3]}/>
+        </div>
+       )
+    }
 }
 
-export default App;
