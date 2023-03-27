753c7474156b0fce3c3f659650674c10b3e089d4
jibola2588 <jibolafawole258@gmail.com>
2023-03-27 10:50:11 +0100

    added hook class to the repo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 // import './appss'.c
 import style from './app.module.css'
 import {Container,Header,Footer} from './app.styles'
-import ComponentA from './componentA'
-
+import Counter from './classes/27-03-23'
 
 const App = () => {
+  const [showComp,setShowComp] = useState(true)
+
   return (
    <Container>
-      {/* <Header>Heading one</Header>
-      <Footer>We are here</Footer> */}
-      <ComponentA />
+    <p>Main Component</p>
+    {showComp &&   <Counter />}
+    <button 
+    onClick={() => setShowComp(false)}
+    >hide comp</button>
+    
    </Container>
   );
 }
---
+++ src/classes/27-03-23.jsx
@@ -0,0 +1,37 @@
+import {Component} from 'react'
+
+
+export default class Counter extends Component{
+    constructor(){ 
+        super()
+        this.state = { 
+           count : 0
+        } 
+    }
+    componentDidMount(){
+        setInterval(() => { 
+            this.updateTime()
+        },1000)
+    }
+    componentWillUpdate(){ 
+       document.title = this.state.count
+    }
+    componentWillUnmount(){ 
+        document.title = 0
+    }
+    
+    updateTime(){
+        this.setState({ 
+           count : this.state.count +1
+        })
+    }
+    render(){ 
+        return ( 
+         <>
+           <p>component A</p>
+              <p>count -- {this.state.count} </p>
+
+         </>
+        )
+    }
+}
